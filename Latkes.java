76b0f2313499fbc2202e7eea71bf615958f77c15
71619997a <marksg07@gmail.com>
2016-03-31 14:32:59 -0400

    Add generic typing for Latkes

--- Latkes.java
+++ Latkes.java
@@ -1,36 +1,69 @@
 /*****************************************************
  * skeleton for class Latkes
- * Implements stack of Strings using array as underlying container.
- * new in Version 2: typed for generics
+ * Implements a stack of Strings using an array as underlying container.
  *****************************************************/
 
-public class Latkes<T> implements Stack<T> {
+public class Latkes<T> implements Stack<T>{
 
     private T [] _stack;
     private int _stackSize;
 
 
     //constructor
-    public Latkes() 
-	{ 
-	    //typecasting hard to avoid here:
-	    _stack = ( T[] )new Object[42]; 
-	    //...
+    public Latkes ( int size ) 
+    { 
+	_stack = (T[])new Object[size];
+	_stackSize = 0;
+    }
+
+    //means of insertion
+    public void push( T s ) 
+    { 
+	//if necessary, create more storage space
+	if ( isFull() ) {
+	    T[] temp = (T[])new Object[ _stack.length * 2 ];	
+	    for( int i = 0; i < _stack.length; i++ ) {
+		temp[i] = _stack[i];
+	    }
+	    _stack = temp; //old _stack freed by JGC
+	}
+	//add new element at end of array
+	//Q: Why is this more efficient than inserting at front?
+	_stack[_stackSize] = s;
+	_stackSize++;
     }
 
 
-    //overloaded constructor allows for intial capacity declaration
-    public Latkes( int size ) 
-	{ 
+    //means of removal
+    public T pop( ) 
+    { 
+	if ( isEmpty() )  return null;
+	T retStr = _stack[ _stackSize-- - 1 ];
+	return retStr;
+    }
 
+    public T peek() {
+	if(isEmpty()) return null;
+	return _stack[_stackSize - 1];
+    }
+
+    //chk for emptiness
+    public boolean isEmpty() 
+    { 
+	return _stackSize == 0; 
+    }
+
+
+    //chk for fullness
+    public boolean isFull() 
+    {
+	return _stackSize >= _stack.length; 
     }
 
 
     //main method for testing
     public static void main( String[] args ) {
-
-	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
-
+	
 	Latkes<String> tastyStack = new Latkes<String>(10);
 
 	tastyStack.push("aoo");
@@ -47,47 +80,35 @@ public class Latkes<T> implements Stack<T> {
 	tastyStack.push("cachoo");
 
 	//cachoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//coocoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//joo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//ioo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//hoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//goo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//foo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//eoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//doo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//coo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//boo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 	//aoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
 
 	//stack empty by now; SOP(null)
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
 	System.out.println( tastyStack.pop() );
-          ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+	  ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
     }//end main()
 
 }//end class Latkes
